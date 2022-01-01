871b108553e75841af022ca2ebe2d051d942404d
Loc Chuong <73679364+Locchuong96@users.noreply.github.com>
2022-01-01 18:00:28 +0700

    Add files via upload

---
+++ Bash Scripts/3-comments/comment.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# this is a comment
+echo "Learn how to add comment in a .sh file"
---
+++ Bash Scripts/3-comments/comments.sh
@@ -0,0 +1,8 @@
+#! /bin/bash 
+: '
+this is a comment
+this is a comment
+this is a comment
+'
+
+echo "Learning how to use multiline comment"
