710e9f476e83e751fa17a35d0e0f40acd6f2a2e6
Loc Chuong <73679364+Locchuong96@users.noreply.github.com>
2022-01-01 17:46:02 +0700

    Add files via upload

---
+++ Bash Scripts/2-redirect_to_file/appendScript.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "This script will append the content in text.txt, press ctrl + d to finish"
+	
+cat >> text.txt
---
+++ Bash Scripts/2-redirect_to_file/outScript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "Write something and press ctrl + d to finsh"
+
+cat > out.txt
