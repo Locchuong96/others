fc6b4452018349909f0b61cba232a4e37bfaa181
locchuong <locchuong123@gmail.com>
2023-08-30 22:17:29 +0700

    Add files via upload

--- C++/CPPs/q39.cpp
+++ C++/CPPs/q39.cpp
@@ -19,15 +19,59 @@ class Circle{
 // intermediate class
 class Sphere:public Circle{
     public:
+    float volume;
     // parameterized constuctor
     Sphere(float radius):Circle(radius){
         // constructor calling statement for base class
         this->radius = radius;
     }
+    ~Sphere(){}
+    void sphere_volume(){
+        volume = (4.0/3)*PI*radius*radius*radius;
+        cout << "Volume of Sphere: " << volume << endl;
+    }
+};
+
+class Cylinder: public Sphere{
+    public:
+    float volume;
+    float height;
+    Cylinder(float radius,float height):Sphere(radius){
+        this->height = height;
+    }
+    Cylinder(float radius):Sphere(radius){
+        this->radius = radius;
+    }
+    ~Cylinder(){}
+    void cylinder_volume(){
+        volume = height * PI * radius * radius;
+        cout << "Volume of Cylinder: " << volume << endl;
+    }
 };
 
 int main(){
-    Circle c1(6);
+    int h;
+    int r;
+    cout << "Enter radius value" << endl;
+    cin >> r;
+    Circle c1(r);
     c1.circle_area();
+
+    cout << "Enter radius value" << endl;
+    cin >> r;
+    Sphere s1(4);
+    s1.sphere_volume();
+
+    cout << "Enter radius and height value" << endl;
+    cin >> r >> h;
+
+    Cylinder l1(r,h);
+    l1.cylinder_volume();
+    l1.circle_area();
+    
+    Cylinder l2(2);
+    l2.cylinder_volume();
+    l2.circle_area();
+    
     return 0;
 }
