4a994b6bd81b38664ebb9f4e03f569b1e5d43cee
locchuong <locchuong123@gmail.com>
2024-02-29 05:40:30 +0700

    Add files via upload

---
+++ C++/cpps/build/CMakeFiles/cpps.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpps.dir/main.cpp.o"
+  "CMakeFiles/cpps.dir/main.cpp.o.d"
+  "cpps"
+  "cpps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
